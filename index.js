3a4576e6ea6293cd5204d3c1e1abcd28f3d7930a
pselden <pselden4@gmail.com>
2017-11-13 10:18:38 -0500

    Show issue with apollo 0.0.18 with infinite fetch loop

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-const { autorun, extendObservable, computed, act, toJS } = require('mobx');
-const { ApolloClient, createNetworkInterface } = require('apollo-client');
-const { query } = require('mobx-apollo');
 const gql = require('graphql-tag');
+const { ApolloClient, createNetworkInterface } = require('apollo-client');
+const { autorun, extendObservable, toJS } = require('mobx');
+const graphql = require('mobx-apollo').default;
+
 global.fetch = require('node-fetch');
 
-const client = new ApolloClient({
-  networkInterface: createNetworkInterface({
-    uri: 'https://api.graph.cool/simple/v1/cj7c7kt9m08oe0198x4sk8zgy',
-    dataIdFromObject: o => o.id
-  })
+const networkInterface = createNetworkInterface({
+  uri: 'https://api.graph.cool/simple/v1/cj7c7kt9m08oe0198x4sk8zgy',
+  dataIdFromObject: o => o.id
 });
+const client = new ApolloClient({
+  networkInterface
+});
+
+networkInterface.use([
+  {
+    applyMiddleware(req, next) {
+      console.log('making request');
+      next();
+    }
+  }
+]);
 
 const allPosts = gql`
   {
@@ -20,54 +31,28 @@ const allPosts = gql`
   }
 `;
 
-const singlePost = gql`
-  query post($id: ID!) {
-    Post(id: $id) {
-      id
-      title
-    }
-  }
-`;
-
 class PostsStore {
   constructor() {
-    extendObservable(this, { postId: 'cj7c8rb0x1ndu01842w34gxby' });
     extendObservable(this, {
-      get postNames() {
-        const posts = this.allPosts.data || [];
-        try {
-          return posts.map(p => p.title);
-        } catch (e) {
-          console.log('Error getting post titles:', e.message);
-          return [];
-        }
+      get allPosts() {
+        const query = graphql({
+          client,
+          query: allPosts,
+          fetchPolicy: 'network-only'
+        });
+        const posts = query.data.allPosts || [];
+        posts.unshift(this.firstPost);
+        return posts;
+      },
+      firstPost: {
+        id: '0',
+        title: 'Empty Post'
       }
     });
-
-    query(this, 'allPosts', {
-      client,
-      query: allPosts
-    });
-
-    query(this, 'currentPost', {
-      client,
-      query: singlePost,
-      variables: this.currentPostVariables
-    });
-  }
-
-  get currentPostVariables() {
-    return {
-      id: this.postId
-    };
-  }
-
-  setCurrentPost(postId) {
-    this.postId = postId;
   }
 }
 
-const postStore = new PostsStore();
-autorun(() => console.log('all posts:', toJS(postStore.allPosts.data)));
-autorun(() => console.log('current post', toJS(postStore.currentPost.data)));
-autorun(() => console.log('post names', postStore.postNames));
+const postsStore = new PostsStore();
+setTimeout(function() {
+  autorun(() => console.log('all posts', toJS(postsStore.allPosts)));
+}, 2000);
